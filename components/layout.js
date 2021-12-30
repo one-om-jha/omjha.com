b2f4ebcf78a8ac9a78c559fa1161feb1db2427ef
Om Jha <omjha@oms-macbook-air.lan>
2021-12-29 20:09:48 -0500

    initial next.js and site setup

---
+++ components/layout.js
@@ -0,0 +1,35 @@
+import Head from 'next/head'
+import Link from 'next/link'
+import styles from './layout.module.css'
+
+const name = "Om Jha"
+export const siteTitle = "omjha.com"
+
+export default function Layout({ children, home }) {
+    return (
+        <div class={styles.body}>
+            <Head>
+                <title>{siteTitle}</title>
+            </Head>
+            <div class={styles.container}>
+                <nav class={styles.nav}>
+                    <Link href="/"><a>home</a></Link>
+                    <Link href="/"><a>design</a></Link>
+                    <Link href="/"><a>art</a></Link>
+                    <Link href="/"><a>writing</a></Link>
+                </nav>
+                <div class={styles.content}>
+                    <main>
+                        {children}
+                    </main>
+                    <hr/>
+                    <div class={styles.footer}>
+                        <Link href="/"><a>twitter</a></Link>{' | '}
+                        <Link href="/"><a>behance</a></Link>{' | '}
+                        <Link href="/"><a>instagram</a></Link>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
